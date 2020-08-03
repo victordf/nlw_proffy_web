f8dedd64523b6053194f5bdb3bd7e6044c316c8f
Victor Martins Machado <victormachado90@gmail.com>
2020-08-03 12:09:39 -0300

    Criação dos componentes gerais e das páginas study e give-classes

---
+++ src/pages/TeacherForm/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PageHeader from '../../components/PageHeader'
+
+import './style.css'
+
+export default function TeacherForm() {
+  return (
+    <div id="page-teacher-form">
+      <PageHeader title="Que bom que você quer dar aulas" />
+    </div>
+  )
+}
