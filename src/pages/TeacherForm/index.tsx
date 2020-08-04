7cb8756da48d72838298f126a4232d4d91dd6d8a
Victor Martins Machado <victormachado90@gmail.com>
2020-08-03 22:40:29 -0300

    Criação do protótipo de cadastro de professor

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -2,11 +2,103 @@ import React from 'react'
 import PageHeader from '../../components/PageHeader'
 
 import './style.css'
+import TextField from '../../components/TextField'
+import warningIcon from '../../assets/images/icons/warning.svg'
 
 export default function TeacherForm() {
   return (
     <div id="page-teacher-form">
-      <PageHeader title="Que bom que você quer dar aulas" />
+      <PageHeader title="Que bom que você quer dar aulas">
+        <p>O primeiro passo, é preencher esse formulário</p>
+      </PageHeader>
+
+      <main>
+        <form id="form-teacher">
+          <fieldset>
+            <legend><span>Seus Dados</span></legend>
+
+            <TextField 
+              id="name"
+              label="Nome Completo"
+            />
+
+            <TextField 
+              id="link-foto"
+              label="Link da sua foto (comece com //http)"
+            />
+
+            <TextField 
+              id="whatsapp"
+              label="Whatsapp (somente número)"
+            />
+
+            <TextField 
+              id="biografia"
+              label="Biografia"
+              type="textarea"
+              rows={5}
+              cols={12}
+            />
+          </fieldset>
+
+          <fieldset>
+            <legend><span>Sobre a aula</span></legend>
+
+            <TextField 
+              id="subject"
+              label="Matéria"
+            />
+
+            <TextField 
+              id="value-hora"
+              label="Custo da sua hora por aula (em R$)"
+            />
+          </fieldset>
+
+          <fieldset>
+            <legend>
+              <div>
+                <span>Horários disponíveis</span>
+                <button type="button">Novo horário</button>
+              </div>
+            </legend>
+
+            <div id="grid-horarios">
+              <div className="horarios-group">
+                <TextField 
+                  id="week-day"
+                  className="horario-item"
+                  label="Dia da semana"
+                />
+
+                <TextField 
+                  id="time-init"
+                  className="horario-item"
+                  label="Das"
+                />
+
+                <TextField 
+                  id="time-finish"
+                  className="horario-item"
+                  label="Até"
+                />
+              </div>
+            </div>
+          </fieldset>
+
+          <footer>
+            <div id="div-aviso">
+              <img src={warningIcon} alt="Aviso"/>
+              <div>
+                <span>Importante!</span>
+                <span>Preencha todos os dados</span>
+              </div>
+            </div>
+
+            <button type="button">Salvar cadastro</button>
+          </footer>
+        </form>
+      </main>
     </div>
   )
 }
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -4,26 +4,27 @@ import PageHeader from '../../components/PageHeader'
 
 import './style.css'
 import TeacherItem from '../../components/TeacherItem'
+import TextField from '../../components/TextField'
 
 export default function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis.">
         <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Matéria</label>
-            <input type="text" id="subject"/>
-          </div>
+          <TextField 
+            id="subject"
+            label="Matérias"
+          />
 
-          <div className="input-block">
-            <label htmlFor="week_day">Dia da semana</label>
-            <input type="text" id="week_day"/>
-          </div>
+          <TextField 
+            id="week_day"
+            label="Dia da Semana"
+          />
 
-          <div className="input-block">
-            <label htmlFor="time">Hora</label>
-            <input type="text" id="time"/>
-          </div>
+          <TextField 
+            id="time"
+            label="Hora"
+          />
         </form>
       </PageHeader>
 
