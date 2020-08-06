6cc83d135c1460bc1a74c3228d33eda28e5bca59
Victor Martins Machado <victormachado90@gmail.com>
2020-08-06 09:39:07 -0300

    Correção do botão do whatsapp

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -42,7 +42,7 @@ const TeacherItem: React.FC<IProps> = ({ teacher }) => {
           Preço/hora
           <strong>R$ {teacher.cost}</strong>
         </p>
-        <a target="_blank" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
+        <a target="_blank" rel="noopener noreferrer" onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
           <img src={whatsappIcon} alt="Whatsapp"/>
           Entrar em contato
         </a>
